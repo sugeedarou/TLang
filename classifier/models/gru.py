3f4dd3a0a5bcbfd7b85594b39c7f7bbc00026fb9
sugeedarou <6258568+sugeedarou@users.noreply.github.com>
2022-12-08 21:57:39 +0100

    fix path

--- classifier/models/gru.py
+++ classifier/models/gru.py
@@ -11,9 +11,9 @@ class GRUModel(nn.Module):
         self.rnn = nn.GRU(input_size=Dataset.characters_count+1,
                           hidden_size=256,
                           num_layers=3,
-                          bidirectional=True, dropout=0.1)
+                          bidirectional=True, dropout=0.5)
         self.fc = nn.Linear(self.rnn.hidden_size*2, self.output_size)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, batch):
         out, _ = self.rnn(batch)
