5f356e4081e56cad85952c7dc294029d8f6d3398
sugeedarou <6258568+sugeedarou@users.noreply.github.com>
2022-12-09 18:00:45 +0100

    reduce dataset characters

--- classifier/models/gru.py
+++ classifier/models/gru.py
@@ -11,13 +11,13 @@ class GRUModel(nn.Module):
         self.rnn = nn.GRU(input_size=Dataset.characters_count+1,
                           hidden_size=256,
                           num_layers=3,
-                          bidirectional=True, dropout=0,
+                          bidirectional=True, dropout=0.1,
                           batch_first=True)
         self.fc = nn.Linear(self.rnn.hidden_size*2, self.output_size)
         self.dropout = nn.Dropout(0.1)
 
     def forward(self, batch):
         out, _ = self.rnn(batch)
-        # out = self.dropout(out)
+        out = self.dropout(out)
         out = self.fc(out[:, -1])
         return out
--- preprocessing/6_cleanup_datasets.py
+++ preprocessing/6_cleanup_datasets.py
@@ -1,23 +1,12 @@
-import hanzidentifier
 import re
 import emoji
 import pandas as pd
 
 def clean_char(c):
-    if hanzidentifier.is_simplified(c):
-        return '这'
-    elif hanzidentifier.has_chinese(c):
-        return '這'
-    if u'\u3040' <= c <= u'\u30FF' or u'\u31F0' <= c <= u'\u31FF' or 'ｧ' <= c <= 'ﾝ': # japanese letters
-        return 'あ'
-    if u'\u1100' <= c <= u'\u11FF' or u'\u3130' <= c <= u'\u318F' or u'\uAC00' <= c <= u'\uD7AF': # korean letters
-        return '가'
-    if (u'\u02B0' <= c <= u'\u036F' or # spacing moodifier letters & combining diacritical marks
-        u'\u2000' <= c <= u'\u2BFF' or # different symbols
-        u'\u1D100' <= c <= u'\u1D1FF' or # musical symbols
-        u'\u1D400' <= c <= u'\u1D7FF' or # math alphanumeric symbols
-        '!' <= c <= '@' or '[' <= c <= '`' or '{' <= c <= '½' or '！' <= c <= '＠' or
-        '［' <= c <= '｀' or '｛' <= c <= '･'): # different symbols, numbers
+    if (u'\u2000'  <= c <= u'\u2BFF'  or  # different symbols
+        u'\u1D100' <= c <= u'\u1D1FF' or  # musical symbols
+        u'\u1D400' <= c <= u'\u1D7FF' or  # math alphanumeric symbols
+        c in '!"#$%&\'()*+,-./0123456789:;=?@[]\\^_´`}{§$°<>|'):
         return ''
     return emoji.replace_emoji(c, replace='')
 
@@ -41,7 +30,7 @@ def cleanup_characters():
                 characters.add(c)
     characters = list(characters)
     characters.sort()
-    with open('data/raw/characters.csv', 'w', encoding='utf-8', newline='') as f_out:
+    with open('data/raw/characters_clean.csv', 'w', encoding='utf-8', newline='') as f_out:
         for c in characters:
             f_out.write(c + '\n')
 
