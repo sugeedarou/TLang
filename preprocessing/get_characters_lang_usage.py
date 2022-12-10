ebd5d50ffee8057f234c42b47e44e86d19002f92
sugeedarou <6258568+sugeedarou@users.noreply.github.com>
2022-12-10 14:15:20 +0100

    improve language character frequency script

--- preprocessing/get_characters_lang_usage.py
+++ preprocessing/get_characters_lang_usage.py
@@ -1,21 +1,33 @@
 import pandas as pd
 
 chars = {}
+samples_per_lang = {}
 df = pd.read_csv('data/raw/train_val_reduced.csv').sort_values(by='lang')
 for index, row in df.iterrows():
     text = row['text']
     lang = row['lang']
-    for c in set(text):
-        if not c in chars:
-            chars[c] = set()
-        chars[c].add(lang)
+    if not lang in samples_per_lang:
+        samples_per_lang[lang] = 0
+    samples_per_lang[lang] += 1
 
-chars = dict([(c,len(chars[c])) for c in chars])
-sorted_chars = dict(sorted(chars.items(), key=lambda x:x[1], reverse=True))
+    for c in set(text):
+        if not lang in chars:
+            chars[lang] = {}
+        if not c in chars[lang]:
+            chars[lang][c] = 0
+        chars[lang][c] += 1
+
+for lang in chars:
+    for c in chars[lang]:
+        chars[lang][c] = round(chars[lang][c] / samples_per_lang[lang] * 10000) / 100
+    sorted_lang = dict(sorted(chars[lang].items(), key=lambda x:x[1], reverse=True))
+    chars[lang] = sorted_lang
 
 with open('data/characters_lang_usage.tsv', 'w', encoding='utf-8', newline='') as f_out:
-    for c in sorted_chars:
-        count = sorted_chars[c]
-        f_out.write(f'{c}\t{count}\n')
+    for lang in chars:
+        f_out.write(f'\n---{lang}---\n')
+        for c in chars[lang]:
+            count = chars[lang][c]
+            f_out.write(f'{c}\t{count}%\n')
 
 print('done')
