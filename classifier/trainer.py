5966322862e2c90d024a6c121028839aded307aa
sugeedarou <linus.schilpp@gmail.com>
2023-01-29 21:49:09 +0100

    fix training, add metrics

--- classifier/trainer.py
+++ classifier/trainer.py
@@ -36,8 +36,8 @@ class Trainer():
             'rec': Recall(**metric_args).to(device),
             'f1': F1Score(**metric_args).to(device),
             'acc': Accuracy(**metric_args).to(device),
-            'confmat': ConfusionMatrix(task=self.task, num_classes=self.num_classes)
         })
+        self.confmat_metric = ConfusionMatrix(task=self.task, num_classes=self.num_classes).to(device)
         # self.f1_metric = F1(num_classes=self.dataset.num_classes)
         # optional parameters
         self.max_epochs = max_epochs
@@ -56,10 +56,9 @@ class Trainer():
                 # train
                 train_tepoch.set_description(f'Epoch {epoch} / {self.max_epochs}')
                 if epoch > 1:
-                    train_tepoch.set_postfix_str(
-                        f'train_loss={self.format_num_for_print(train_loss)}, '
-                        f'val_loss={self.format_num_for_print(val_loss)}, '
-                        f'val_accuracy={self.format_num_for_print(val_accuracy)}')
+                    log_str_train = self.get_log_metrics_str('train', train_loss, [])
+                    log_str_val   = self.get_log_metrics_str('val', val_loss, val_metrics)
+                    train_tepoch.set_postfix_str(f'{log_str_train}, {log_str_val}')
                 train_loss = self.training_epoch(train_tepoch)
                 # validate
                 if epoch == self.max_epochs:
@@ -67,18 +66,6 @@ class Trainer():
                 with tqdm(val_loader) as val_tepoch: 
                     val_tepoch.set_description('Validating')
                     val_loss, val_metrics = self.validation_epoch(val_tepoch)
-                    val_accuracy = val_metrics['acc'].item()
-                    
-                
-    def test(self):
-        test_loader = self.dataloader.test_dataloader()
-        with torch.no_grad(), tqdm(test_loader) as tepoch: 
-            tepoch.set_description('Testing')
-            for i, batch in enumerate(tepoch):
-                loss, accuracy = self.validation_step(batch, i)
-            
-            confmat = self.confmat_metric.compute()
-            show_confusion_matrix(confmat)
 
     def training_epoch(self, data):
         losses = torch.zeros(len(data))
@@ -105,10 +92,10 @@ class Trainer():
             for i, batch in enumerate(data):
                 losses[i] = self.validation_step(batch, i)
             loss = torch.mean(losses).item()
-            val_metrics = self.val_metrics.compute()
             self.lr_scheduler.validation_epoch_end({'val_loss': loss})
+            metrics = self.val_metrics.compute()
             self.val_metrics.reset()
-            return loss, val_metrics
+            return loss, metrics
 
     def validation_step(self, batch, _step_index):
         _, _, labels, texts = batch
@@ -118,11 +105,36 @@ class Trainer():
         preds = out.argmax(1)
         self.val_metrics(preds, labels)
         return loss
+    
+    def test(self):
+        test_loader = self.dataloader.test_dataloader()
+        with torch.no_grad(), tqdm(test_loader) as tepoch: 
+            tepoch.set_description('Testing')
+            loss, metrics = self.test_epoch(tepoch)
+            log_str   = self.get_log_metrics_str('test', loss, metrics)
+            print(log_str)
+            confmat = self.confmat_metric.compute().cpu()
+            show_confusion_matrix(confmat)
+
+    def test_epoch(self, data):
+        with torch.no_grad():
+            losses = torch.zeros(len(data))
+            for i, batch in enumerate(data):
+                losses[i] = self.test_step(batch, i)
+            loss = torch.mean(losses).item()
+            metrics = self.test_metrics.compute()
+            self.test_metrics.reset()
+            return loss, metrics
 
     def test_step(self, batch, _):
         _, _, labels, texts = batch
-        loss, accuracy = self.calculate_metrics(batch, mode='test')
-        return loss, accuracy
+        texts = texts.to(self.device)
+        labels = labels.to(self.device)
+        out, loss = self.predict_with_model(texts, labels)
+        preds = out.argmax(1)
+        self.test_metrics(preds, labels)
+        self.confmat_metric(preds, labels)
+        return loss
 
     def predict_with_model(self, texts, labels):
         labels_one_hot = Fun.one_hot(labels, num_classes=self.num_classes).float().to(self.device)
@@ -140,7 +152,13 @@ class Trainer():
         print(f"Using {device} device")
         return device
 
+    def get_log_metrics_str(self, type_name, loss, metrics):
+        log_metrics_str = f'{type_name}_loss={self.format_num_for_print(loss)}'
+        for i, metric in enumerate(metrics):
+            log_metrics_str += f', {type_name}_{metric}={self.format_num_for_print(metrics[metric].item())}'
+        return log_metrics_str
+
     def format_num_for_print(self, n):
         if n > 1:
-            return floor(n*100) * 0.01
-        return floor(n*1000) * 0.001
+            return floor(n*100) / 100
+        return floor(n*1000) / 1000
