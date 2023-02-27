7f11a60a48a499a01e588a187746159072934a1d
sugeedarou <linus.schilpp@gmail.com>
2023-02-27 23:28:28 +0100

    rewrite gru

--- classifier/models/rnns.py
+++ classifier/models/rnns.py
@@ -22,19 +22,25 @@ class BiGRU(nn.Module):
         '''
         seq_length = input.size(0)
         batch_size = input.size(1)
-        h_f = [torch.zeros(batch_size, self.hidden_size, device=self.device)
+        h_f = [torch.zeros(seq_length, batch_size, self.hidden_size, device=self.device)
                for _ in range(self.num_layers)]
-        h_b = [torch.zeros(batch_size, self.hidden_size, device=self.device)
+        h_b = [torch.zeros(seq_length, batch_size, self.hidden_size, device=self.device)
                for _ in range(self.num_layers)]
         out = torch.zeros((seq_length, batch_size, 2*self.hidden_size), device=self.device)
 
-        for t in range(seq_length):
-            x = input[t]
-            for i in range(self.num_layers):
-                h_f[i] = self.forward_cells[i](x, h_f[i])
-                h_b[i] = self.backward_cells[i](x.flip(0), h_b[i])
-                x = torch.cat((h_f[i], h_b[i]), dim=1)
-                if self.dropout > 0:
-                    x = nn.Dropout(p=self.dropout)(x)
-            out[t] = x
+        last_hidden = input
+
+        for i in range(self.num_layers):
+            for t in range(seq_length):
+                x1 = last_hidden[t]
+                x2 = last_hidden[-(t+1)]
+                h_f[i][t] = self.forward_cells[i](x1, h_f[i][t-1])
+                h_b[i][t] = self.backward_cells[i](x2, h_b[i][t-1])
+            
+            last_hidden = torch.cat((h_f[i], h_b[i].flip(0)), dim=2)
+
+            if i == self.num_layers - 1:
+                out = last_hidden
+            if self.dropout > 0:
+                last_hidden = nn.Dropout(p=self.dropout)(last_hidden)
         return out
